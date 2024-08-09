f481ab2a46727f6ff13b8665425f8f9c1942b301
Sutapa Das <sutapa.das835@gmail.com>
2024-08-09 23:58:50 +0530

    temparature converter

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const Celcius = document.querySelector('#celcius')
+
 const Kelvin = document.querySelector('#kelvin')
+
 const Farenhite = document.querySelector('#farenheit')
+
+console.log(Celcius, Fahrenheit, Kelvin);
